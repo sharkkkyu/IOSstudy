f3b7bca63d8b98d44b8cad9cafa3590e4d82a18c
yusongmin <yusongmin02@meituan.com>
2024-08-23 17:42:37 +0800

    提交

--- 第一个oc类/main.m
+++ 第一个oc类/main.m
@@ -1,15 +1,15 @@
 //
 //  main.m
-//  FollowtheClass
+//  第一个oc类
 //
 //  Created by Songmin Yu on 2024/8/23.
 //
 
 #import <Foundation/Foundation.h>
-int main(int argc, const char * argv[]) { 
+
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
         NSLog(@"Hello, World!");
     }
     return 0;
