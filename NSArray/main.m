92fe21c8d767f4b4b2c32b582f73a313dc54704b
yusongmin <yusongmin02@meituan.com>
2024-08-29 19:57:05 +0800

    feat

---
+++ NSArray/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  NSArray
+//
+//  Created by Songmin Yu on 2024/8/29.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+//        NSArray *arr = [NSArray arrayWithObjects:@"123",@"455",@"$!4",nil];
+//        NSLog(@"%@",arr);
+//        [arr count];
+//        [arr lastObject];
+//        [arr firstObject];
+//        [arr objectAtIndex:1];
+//        [arr containsObject:@"123"];
+//        NSArray *arr = @[@"123",@"34",@"214"];
+//        NSLog(@"%@",arr[0]);
+//        NSString *str = [arr componentsJoinedByString:@"---"];
+//        NSLog(@"%@",str);
+    }
+    return 0;
+}
