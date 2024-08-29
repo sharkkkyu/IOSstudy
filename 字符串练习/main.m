bb4b5874a679fe3c685d572a45682d73d9834447
yusongmin <yusongmin02@meituan.com>
2024-08-29 18:56:11 +0800

    字符串

---
+++ 字符串练习/main.m
@@ -0,0 +1,108 @@
+//
+//  main.m
+//  字符串练习
+//
+//  Created by Songmin Yu on 2024/8/29.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+//        NSError *error = nil;
+//        NSString *str = [NSString stringWithContentsOfFile:@"/Users/songminyu/Desktop/1.doc" encoding:NSUTF8StringEncoding error:&error];
+//        if (error == nil)
+//        {
+//            NSLog(@"%@",str);
+//        } else {
+//            NSLog(@"error= %@",[error localizedDescription]);
+//        }
+        
+        
+//        [str writeToFile:<#(nonnull NSString *)#> atomically:<#(BOOL)#> encoding:<#(NSStringEncoding)#> error:<#(NSError *__autoreleasing  _Nullable * _Nullable)#>]; // 将字符串写入到文件
+        
+        
+//        NSString *path = @"http://www.baidu.com";
+//        NSURL *url = [NSURL URLWithString:path];
+//        NSString *str = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+//        NSLog(@"%@",str);
+        
+//        字符串比较
+//        NSString *str1 = @"123";
+//        NSString *str2 = [NSString stringWithFormat:@"123"];
+//        BOOL flag = [str1 isEqualTo: str2];
+//        NSLog(@"%i",flag);
+        
+        
+//        字符串搜索
+//        判断是否以xxx开头
+        NSString *str = @"ysm is a handsome boy";
+//        BOOL flag = [str hasPrefix:@"ysm"];
+//        NSLog(@"%i",flag);
+        
+//         判断是否以xxx结尾
+//        BOOL flag = [str hasSuffix:@"boy"];
+//        NSLog(@"%i",flag);
+        
+//        判断是否包含xxx
+//        NSRange range = [str rangeOfString:@"handsome"];
+//        NSLog(@"location = %lu,length = %lu",range.location,range.length);
+        
+//        字符串截取
+//        NSRange range = {1,2};//位置，长度
+//        NSString *str2 = [str substringWithRange:range];
+//        NSLog(@"%@",str2);
+        
+//        [str substringFromIndex:<#(NSUInteger)#>];
+        
+//        替换字符串
+//       NSString *str2 =  [str stringByReplacingOccurrencesOfString:<#(nonnull NSString *)#> withString:<#(nonnull NSString *)#>]
+        
+        
+//        判断是否是绝对路径
+//        if([str isAbsolutePath]);
+        
+        
+//        获取文件路径中的最后一个目录
+//        [str lastPathComponent];
+        
+        
+//        删除文件路径中的最后一个目录
+//        NSString *str2 = [str stringByDeletingLastPathComponent];
+        
+        
+//        给文件路径添加一个目录
+//        NSString *str2 [str stringByAppendingPathComponent:(nonnull NSString *)];
+        
+//        获取路径中文件的扩展名
+//        NSString *str2 = [str pathExtension];
+        
+        
+//        删除路径中文件的扩展名
+//        NSString *str2 = [str stringByDeletingPathExtension];
+        
+        
+//        给文件路径添加一个扩展名
+//        NSString *str2 = [str stringByAppendingString:<#(nonnull NSString *)#>];
+        
+        
+//        将字符串转换为大写
+//        NSString *str2 = [str uppercaseString];
+        
+        
+//        将字符串转换为小写
+//        NSString str2 = [str lowercaseString];
+        
+//        将字符串的首字母转换为大写
+//        NSString *str2 = [str capitalizedString];
+        
+        
+//        字符串与基本数据类型的转换
+//        NSString str = @"110";
+//        int value = [str intValue];
+        
+        
+        
+    }
+    return 0;
+}
