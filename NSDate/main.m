b044728eb3f754e28ab56273fc84fa33bc10c1dd
yusongmin <yusongmin02@meituan.com>
2024-08-30 10:32:46 +0800

    8.30

---
+++ NSDate/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  NSDate
+//
+//  Created by Songmin Yu on 2024/8/30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+//        NSDate *now = [NSDate date]; 保存当前时间
+        
+//        NSDate *date = [now dateByAddingTimeInterval:100]; 在now的基础上追加多少秒
+        
+//        NSTimeZone *zone = [NSTimeZone systemTimeZone]; 获取时区
+        /*
+        NSDate *now = [NSDate now];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"这是 yyyy 年 M 月 d 日 h时 m分 s秒";
+        NSString *res = [formatter stringFromDate:now];
+        NSLog(@"%@",res);
+         */  // 格式化时间格式 
+        
+    }
+    return 0;
+}
